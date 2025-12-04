4dda0dfaf448ddaa7b1ab184d398a11857d6e04e
awkoo <184658409+awkoo@users.noreply.github.com>
2025-12-04 09:26:49 +0800

    KernelSU: 同步版本32262

--- drivers/kernelsu/app_profile.c
+++ drivers/kernelsu/app_profile.c
@@ -24,7 +24,7 @@
 #include "syscall_handler.h"
 #endif
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 7, 0)
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 7, 0) && defined(CONFIG_CC_IS_GCC))
 static struct group_info root_groups = {
 	.usage = REFCOUNT_INIT(2),
 };
@@ -99,7 +99,8 @@ void disable_seccomp(struct task_struct *tsk)
 
 	tsk->seccomp.mode = 0;
 	// 5.9+ have filter_count, but optional.
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 7, 0) || defined(KSU_OPTIONAL_SECCOMP_FILTER_CNT))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 7, 0) ||                          \
+     defined(KSU_OPTIONAL_SECCOMP_FILTER_CNT))
 	atomic_set(&tsk->seccomp.filter_count, 0);
 #endif
 	// some old kernel backport seccomp_filter_release..
@@ -183,4 +184,9 @@ void escape_with_root_profile(void)
 		ksu_set_task_tracepoint_flag(t);
 	}
 #endif
+}
+
+void __maybe_unused escape_to_root_for_init(void)
+{
+	setup_selinux(KERNEL_SU_CONTEXT);
 }
