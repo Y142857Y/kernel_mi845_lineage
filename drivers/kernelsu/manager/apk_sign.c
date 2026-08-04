211ed9ffa8694404c1ab52586d504178dcdebe42
Y142857Y <123782164+Y142857Y@users.noreply.github.com>
2026-08-04 13:58:29 +0800

    Update apk_sign.c

--- drivers/kernelsu/manager/apk_sign.c
+++ drivers/kernelsu/manager/apk_sign.c
@@ -6,46 +6,28 @@
 #include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/string.h>
-
-#include "klog.h"
-#include "ksu.h"
+#include <linux/printk.h>
+#include <linux/namei.h>
+#include <linux/file.h>
+#include <linux/version.h>
 
 #ifndef KSU_MAX_PACKAGE_NAME
 #define KSU_MAX_PACKAGE_NAME 256
 #endif
-
-__attribute__((used)) __attribute__((noinline))
-bool check_v2_signature(char *path, unsigned expected_size,
-			const char *expected_sha256)
+static __always_inline bool check_v2_signature(char *path,
+					       unsigned expected_size,
+					       const char *expected_sha256)
 {
-	(void)path;
-	(void)expected_size;
-	(void)expected_sha256;
-
 	return true;
 }
-
-
 int get_pkg_from_apk_path(char *pkg, const char *path)
 {
-	int len = 0;
-	int i;
-	int pkg_len;
-	const char *last_slash = NULL;
-	const char *second_last_slash = NULL;
-	const char *hyphen = NULL;
-
-	if (!pkg || !path)
+	int len = strlen(path);
+	if (len >= KSU_MAX_PACKAGE_NAME || len < 1)
 		return -1;
 
-	while (path[len])
-		len++;
-
-	if (len <= 0 || len >= KSU_MAX_PACKAGE_NAME)
-		return -1;
-
-
-	for (i = len - 1; i >= 0; i--) {
+	const char *last_slash = NULL, *second_last_slash = NULL;
+	for (int i = len - 1; i >= 0; i--) {
 		if (path[i] == '/') {
 			if (!last_slash)
 				last_slash = &path[i];
@@ -55,68 +37,41 @@ int get_pkg_from_apk_path(char *pkg, const char *path)
 			}
 		}
 	}
-
-	if (!second_last_slash || !last_slash)
+	if (!last_slash || !second_last_slash)
 		return -1;
 
-
-	for (i = 0; second_last_slash[i]; i++) {
-		if (second_last_slash[i] == '-') {
-			hyphen = &second_last_slash[i];
-			break;
-		}
-	}
-
-	if (!hyphen || hyphen > last_slash)
+	const char *last_hyphen = strchr(second_last_slash, '-');
+	if (!last_hyphen || last_hyphen > last_slash)
 		return -1;
 
-
-	pkg_len = (int)(hyphen - second_last_slash - 1);
-
-	if (pkg_len <= 0 || pkg_len >= KSU_MAX_PACKAGE_NAME)
+	int pkg_len = last_hyphen - second_last_slash - 1;
+	if (pkg_len >= KSU_MAX_PACKAGE_NAME || pkg_len <= 0)
 		return -1;
 
-
-	for (i = 0; i < pkg_len; i++)
-		pkg[i] = second_last_slash[i + 1];
-
+	memcpy(pkg, second_last_slash + 1, pkg_len);
 	pkg[pkg_len] = '\0';
-
 	return 0;
 }
-
-
-static bool simple_contains(const char *str, const char *sub)
-{
-	const char *a;
-	const char *b;
-
-	if (!str || !sub)
-		return false;
-
-	while (*str) {
-		a = str;
-		b = sub;
-
-		while (*a && *b && *a == *b) {
-			a++;
-			b++;
-		}
-
-		if (!*b)
-			return true;
-
-		str++;
-	}
-
-	return false;
-}
-
-
 bool is_manager_apk(char *path)
 {
 	if (!path)
 		return false;
-
-	return simple_contains(path, "me.weishu.kernelsu");
+	return strstr(path, "me.weishu.kernelsu") ||
+	       strstr(path, "com.ripes.kernelsu");
 }
+#ifdef CONFIG_KSU_DEBUG
+int ksu_debug_manager_appid = -1;
+
+static int set_dummy(const char *val, const struct kernel_param *kp)
+{
+	return param_set_int(val, kp);
+}
+
+static struct kernel_param_ops dummy_ops = {
+	.set = set_dummy,
+	.get = param_get_int,
+};
+
+module_param_cb(ksu_debug_manager_appid, &dummy_ops,
+		&ksu_debug_manager_appid, S_IRUSR | S_IWUSR);
+#endif
