bda286a593b6eb7043991f48adbd1257638cf9a5
Y142857Y <123782164+Y142857Y@users.noreply.github.com>
2026-08-04 13:24:22 +0800

    Update apk_sign.h

--- drivers/kernelsu/manager/apk_sign.h
+++ drivers/kernelsu/manager/apk_sign.h
@@ -1,6 +1,9 @@
 #ifndef __KSU_H_APK_V2_SIGN
 #define __KSU_H_APK_V2_SIGN
 
+#include <linux/types.h>
+bool check_v2_signature(char *path, unsigned expected_size, const char *expected_sha256);
+
 bool is_manager_apk(char *path);
 int get_pkg_from_apk_path(char *pkg, const char *path);
 
