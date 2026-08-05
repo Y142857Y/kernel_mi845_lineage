10d8c2e40f409537326b3259a5cc1b8d483700a4
Y142857Y <123782164+Y142857Y@users.noreply.github.com>
2026-08-05 16:20:22 +0800

    Update gpucc-sdm845.c

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -282,8 +282,7 @@ static const struct freq_tbl  ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2[] = {
 	F(596000000, P_CRC_DIV,  1, 0, 0),
 	F(675000000, P_CRC_DIV,  1, 0, 0),
 	F(710000000, P_CRC_DIV,  1, 0, 0),
-    F(740000000, P_CRC_DIV,  1, 0, 0),
-    F(800000000 ,P_CRC_DIV,  1, 0, 0),
+    F(780000000, P_CRC_DIV,  1, 0, 0),
     F(833000000, P_CRC_DIV,  1, 0, 0),
 	{ }
 };
@@ -644,9 +643,7 @@ static void gpu_cc_gfx_sdm845_fixup_sdm845v2(void)
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
 		710000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
-		740000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
-		800000000;
+		780000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
 		833000000;
 }
