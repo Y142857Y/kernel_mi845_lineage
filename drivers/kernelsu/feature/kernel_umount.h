d847482c1c2fda6f10673a14dbc8a1233beae405
Y142857Y <123782164+Y142857Y@users.noreply.github.com>
2026-08-30 11:28:54 +0800

    Update kernel_umount.h

--- drivers/kernelsu/feature/kernel_umount.h
+++ drivers/kernelsu/feature/kernel_umount.h
@@ -1,11 +1,23 @@
 #ifndef __KSU_H_KERNEL_UMOUNT
 #define __KSU_H_KERNEL_UMOUNT
 
+#include <linux/types.h>
+#include <linux/list.h>
+#include <linux/rwsem.h>
+#include <linux/cred.h>
+
+void ksu_kernel_umount_init(void);
+void ksu_kernel_umount_exit(void);
+extern bool ksu_webview_zygote_umount_enabled;
+
+// Handler function to be called from setresuid hook
+int ksu_handle_umount(struct cred *new, const struct cred *old);
+
 // for the umount list
 struct mount_entry {
-    char *umountable;
-    unsigned int flags;
-    struct list_head list;
+	char *umountable;
+	unsigned int flags;
+	struct list_head list;
 };
 extern struct list_head mount_list;
 extern struct rw_semaphore mount_list_lock;
