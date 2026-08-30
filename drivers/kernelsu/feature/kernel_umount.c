4f1db420bacd799ceaac065d067f367336243492
Y142857Y <123782164+Y142857Y@users.noreply.github.com>
2026-08-30 11:37:34 +0800

    Update kernel_umount.c

--- drivers/kernelsu/feature/kernel_umount.c
+++ drivers/kernelsu/feature/kernel_umount.c
@@ -69,7 +69,7 @@ static void try_umount(const char *mnt, int flags)
 	ksu_umount_mnt(mnt, &path, flags);
 }
 
-static inline int ksu_handle_umount(struct cred *new, const struct cred *old)
+int ksu_handle_umount(struct cred *new, const struct cred *old)
 {
 	uid_t new_uid = ksu_get_uid_t(new->uid);
 	uid_t old_uid = ksu_get_uid_t(old->uid);
